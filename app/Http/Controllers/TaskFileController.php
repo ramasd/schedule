54be0389cc930fc2c075bfa4818806ecc0798f39
ramasd <ramunas.danupas@gmail.com>
2020-05-06 11:41:35 +0300

    Models ant controllers added

---
+++ app/Http/Controllers/TaskFileController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TaskFile;
+use Illuminate\Http\Request;
+
+class TaskFileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TaskFile  $taskFile
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TaskFile $taskFile)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TaskFile  $taskFile
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TaskFile $taskFile)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TaskFile  $taskFile
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TaskFile $taskFile)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TaskFile  $taskFile
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TaskFile $taskFile)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/TaskScheduleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TaskSchedule;
+use Illuminate\Http\Request;
+
+class TaskScheduleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TaskSchedule  $taskSchedule
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TaskSchedule $taskSchedule)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TaskSchedule  $taskSchedule
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TaskSchedule $taskSchedule)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TaskSchedule  $taskSchedule
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TaskSchedule $taskSchedule)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TaskSchedule  $taskSchedule
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TaskSchedule $taskSchedule)
+    {
+        //
+    }
+}
